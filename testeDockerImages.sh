dd2695cd23439c3d3eb846c238ee75825807b784
SirMath200223 <matheus.osilva@sptech.school>
2024-12-01 17:34:40 -0300

    Criação do .env adiantada no testeDockerImages

--- testeDockerImages.sh
+++ testeDockerImages.sh
@@ -16,6 +16,15 @@ if [ $? -eq 0 ]
     fi
 fi
 
+echo "
+NOME_BUCKET=\"bucket-insight-trip\"
+DB_HOST="it-app-db-1"
+DB_PORT=3306
+DB_NAME=\"InsightTrip\"
+DB_USER=\"root\"
+DB_PASSWORD=123
+"  > Java/.env
+
 # Clonando repositório do banco
 git clone https://github.com/Insight-Trip/MySQL.git
 mv MySQL Banco
@@ -28,12 +37,3 @@ sudo docker build -t minha-imagem-java -f ./Java/Dockerfile .
 sudo docker run --name meu-banco -d -p 3306:3306 -e 'MYSQL_ROOT_PASSWORD=123' minha-imagem-banco
 sudo docker run --name meu-site -d -p 3333:3333 minha-imagem-site
 sudo docker run --name meu-java -d minha-imagem-java
-
-echo "
-NOME_BUCKET=\"bucket-insight-trip\"
-DB_HOST="it-app-db-1"
-DB_PORT=3306
-DB_NAME=\"InsightTrip\"
-DB_USER=\"root\"
-DB_PASSWORD=123
-"  > Java/.env
