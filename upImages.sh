65d154d43728379424f04c4966b99712383be877
SirMath200223 <matheus.osilva@sptech.school>
2024-12-01 17:43:15 -0300

    Corrigido upImages.sh

--- upImages.sh
+++ upImages.sh
@@ -1,7 +1,9 @@
-read "Digite o usuário do DockerHub: " USER
+read -p "Digite o usuário do DockerHub: " USER
 
 sudo docker tag minha-imagem-banco $USER/it_bd:v1
 sudo docker tag minha-imagem-site $USER/it_site:v1
 sudo docker tag minha-imagem-java $USER/it_java:v1
 
-sudo docker push $USER/it_bd:v1 $USER/it_site:v1 $USER/it_java:v1
+sudo docker push $USER/it_bd:v1
+sudo docker push $USER/it_site:v1
+sudo docker push $USER/it_java:v1
